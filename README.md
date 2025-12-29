d6de2e1939145b90098e84d6c3f7f3e5d2fe20cc
Sonia N <sonian@Sonias-MacBook-Air.local>
2025-12-29 20:28:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Netflix-Style Recommendation System
+
+A content-based recommendation system that suggests movies
+based on genre similarity using cosine similarity.
+
+Tech Stack:
+- Python
+- Pandas
+- Scikit-learn
+
+Features:
+- Recommends top-K similar movies
+- Explainable similarity-based logic
---
+++ recommender.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+df = pd.read_csv("data/movies.csv")
+
+vectorizer = TfidfVectorizer(
+    token_pattern=r'[^|]+'
+)
+genre_matrix = vectorizer.fit_transform(df['genre'])
+
+
+similarity = cosine_similarity(genre_matrix)
+
+def recommend(movie_title, top_k=3):
+    if movie_title not in df['title'].values:
+        return "Movie not found"
+
+    idx = df[df['title'] == movie_title].index[0]
+    scores = list(enumerate(similarity[idx]))
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+    recommendations = []
+    for i in scores[1:top_k+1]:
+        recommendations.append(df.iloc[i[0]]['title'])
+
+    return recommendations
+
+# Test
+print(recommend("Inception"))
